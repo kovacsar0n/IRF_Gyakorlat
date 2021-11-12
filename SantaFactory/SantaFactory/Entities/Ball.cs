322d35ef06009fc2dbdc4b5effd269f256af6389
kovacs.aron <kovacs.aron@DESKTOP-KEHCNHC>
2021-11-12 14:37:41 +0100

    óra további része

---
+++ SantaFactory/SantaFactory/Entities/Ball.cs
@@ -0,0 +1,23 @@
+﻿using SantaFactory.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SantaFactory.Entities
+{
+   public class Ball: Toy
+    {
+       
+
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+     
+    }
+}
--- SantaFactory/SantaFactory/Form1.cs
+++ SantaFactory/SantaFactory/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SantaFactory.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,48 @@ namespace SantaFactory
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        private BallFactory _ballFactory;
+
+        public BallFactory BallFactory
+        {
+            get { return _ballFactory; }
+            set { _ballFactory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            BallFactory = new BallFactory();
+
+            var b = BallFactory.CreateNew();
+            Controls.Add(b);
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = BallFactory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveToy();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
